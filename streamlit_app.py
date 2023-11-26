39445435bee2c89be580dfee4108ae8150bebef8
Mikecelium <117662411+Mikecelium@users.noreply.github.com>
2023-11-27 04:11:03 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 from langchain.llms import OpenAI
 
@@ -12,18 +13,11 @@ def generate_response(input_text):
   st.info(llm(input_text))
 
 with st.form('my_form'):
-  
-  text = st.text_area('Enter text:', 'Im editing motion graphics on a ten minute youtube video')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text))
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
     generate_response(text)
-
 
 st.write('Yoooooo Romano NASA Time!')
