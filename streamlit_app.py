11b21fc6cee1f544abdb56c5ec6fd8c3b9319369
Mikecelium <117662411+Mikecelium@users.noreply.github.com>
2023-11-28 02:09:16 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,26 @@ messages = [
     HumanMessage(content="I'd like to understand string theory.")
 ]
 
-chat = ChatOpenAI(
-    openai_api_key = key_1,
-    model='gpt-3.5-turbo'
-)
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
 
-res = chat(messages)
+    chat = ChatOpenAI(
+        openai_api_key = key_1,
+        model='gpt-3.5-turbo'
+    )
 
-st.write(print(res))
+    messages2 = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="Hi AI, how are you today?"),
+    AIMessage(content="I'm great thank you. How can I help you?"),
+    HumanMessage(content= input_text)
+    ]
+
+    res2 = chat(messages2)
+    st.write(res2)
+
+    
 
 
 
@@ -40,18 +52,11 @@ st.title('Project Details')
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'Im making motion graphics fro a 10 minute youtube video')
   submitted = st.form_submit_button('Submit')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
 
 
 
-messages2 = [
-    SystemMessage(content="You are a helpful assistant."),
-    HumanMessage(content="Hi AI, how are you today?"),
-    AIMessage(content="I'm great thank you. How can I help you?"),
-    HumanMessage(content=text)
-]
-
-res2 = chat(messages2)
-st.write(res2)
 
 
 
