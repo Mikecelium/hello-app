5509c9f361a2f559a8b4ae3ddaf17c32a9145ff4
Mikecelium <117662411+Mikecelium@users.noreply.github.com>
2023-11-28 05:57:52 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,6 +56,12 @@ with st.form('my_form'):
   if submitted and key_1.startswith('sk-'):
     generate_response(text)
 
+with st.form('my_form2'):
+  text = st.text_area('Enter text:', 'I'm a video graphic designer')
+  submitted = st.form_submit_button('Submit')
+  if submitted and key_1.startswith('sk-'):
+    generate_response(text)
+
 
 
 
