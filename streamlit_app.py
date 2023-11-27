56ab87c1fe9425befcc418d19139fb9c30918be1
Mikecelium <117662411+Mikecelium@users.noreply.github.com>
2023-11-28 08:24:56 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ messages = [
     HumanMessage(content="I'd like to understand string theory.")
 ]
 
-def generate_response(input_text):
+def generate_response2(input_text):
   
 
     chat = ChatOpenAI(
@@ -42,7 +42,7 @@ def generate_response(input_text):
     return res2
 
 
-def generate_response2(input_text):
+def generate_response3(input_text):
   
 
     chat = ChatOpenAI(
@@ -50,14 +50,14 @@ def generate_response2(input_text):
             model='gpt-3.5-turbo'
     )
     
-    messages2 = [
+    messages3 = [
     SystemMessage(content="You are a helpful assistant trying to create questions to narrow down the cost and efficiency of a worker/freelancer, continue asking questions until you have a reasonable idea of what the person is capable of and what resources they have avaliable, ask the most important questions first. Once the User inputs 'Finished' output the project description and answers to questions in a way compatible with prompting an LLM to produce a cost when linked with a project description "),
     HumanMessage(content="Hi AI, how are you today?"),
     AIMessage(content="I'm great thank you. Can you tell me more about the project?"),
     HumanMessage(content= input_text)
     ]
     
-    res2 = chat(messages2)
+    res2 = chat(messages3)
     st.write(res2.content)
 
     return res2
@@ -75,13 +75,18 @@ with st.form('my_form'):
   text = st.text_area('Enter text:', 'Im making motion graphics fro a 10 minute youtube video')
   submitted = st.form_submit_button('Submit')
   if submitted and key_1.startswith('sk-'):
-    generate_response(text)
+    res = generate_response2(text)
+
+    messages2.append(res)
+    
 
 with st.form('my_form2'):
   text = st.text_area('Enter text:', 'Im a video graphic designer')
   submitted = st.form_submit_button('Submit')
   if submitted and key_1.startswith('sk-'):
-    generate_response2(text)
+    res = generate_response3(text)
+
+    messages3.append(res)
 
 
 
