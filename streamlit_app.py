5d48ca245ba3a9d034f32848a0bca80da63ffa6b
Mikecelium <117662411+Mikecelium@users.noreply.github.com>
2023-11-28 11:50:57 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,32 @@ from langchain.schema import (
 
 from langchain.chat_models import ChatOpenAI
 
+
+
+from shapely.geometry import Point, Polygon
+import geopandas as gpd
+import pandas as pd
+import geopy
+
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+
+street = st.sidebar.text_input("Street", "75 Bay Street")
+city = st.sidebar.text_input("City", "Toronto")
+province = st.sidebar.text_input("Province", "Ontario")
+country = st.sidebar.text_input("Country", "Canada")
+
+geolocator = Nominatim(user_agent="GTA Lookup")
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+location = geolocator.geocode(street+", "+city+", "+province+", "+country)
+
+lat = location.latitude
+lon = location.longitude
+
+map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
+
+st.map(map_data) 
+
 key_1 = (os.environ["key"])
 
 
